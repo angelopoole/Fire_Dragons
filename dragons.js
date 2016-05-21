a96e0f08f4bcc4070d8150f87ecaae6162bc1f98
angelopoole <8thgenwolf@gmail.com>
2016-05-21 17:45:52 +0000

    transitions almost ready for implementation

--- dragons.js
+++ dragons.js
@@ -1,14 +1,18 @@
 
-$( document ).ready(function() {
+$(document).ready(function() {
     console.log( "ready!" );
     var count = 0;
-    alert(count)
-//   $("#Like").on('click', function(){
-//     count++
-//     $("#number").text("count")
+
+  $("#like").on('click', function(){
+    count++
+    $("#number").text(count)
 })  
     
+        
+    $("#dislike").on('click', function() {
+        count=count-1
+        $("#number").text(count)
+    })
+    
+    
 });
-
-
-
