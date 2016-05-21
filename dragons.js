51708b0b1f845bb7166e24d1ea87ed53d55d7782
angelopoole <8thgenwolf@gmail.com>
2016-05-21 17:31:50 +0000

    images,nav in progress, inputs in progress, and image slider in progress

---
+++ dragons.js
@@ -0,0 +1,14 @@
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    var count = 0;
+    alert(count)
+//   $("#Like").on('click', function(){
+//     count++
+//     $("#number").text("count")
+})  
+    
+});
+
+
+
